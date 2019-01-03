34316c480a7e850fb95320bc318143de80d1f222
gldev6713 <gldev6713@gmail.com>
2019-01-03 16:12:20 +0700

    remove material-design-icons-iconfont

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -2,7 +2,6 @@ import Vue from 'vue'
 import Vuetify from 'vuetify/lib'
 import 'vuetify/src/stylus/app.styl'
 import 'vue-orgchart/dist/style.min.css'
-import 'material-design-icons-iconfont/dist/material-design-icons.css'
 
 Vue.use(Vuetify, {
   iconfont: 'md',
