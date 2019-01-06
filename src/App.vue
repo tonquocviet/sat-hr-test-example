82f095a1cd978024c212ad25be75933b282a16ce
thuanhd <thuan@htactive.com>
2019-01-06 18:35:02 +0700

    density config

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <Layout>
     <OrgChartWrapper>
-      <HierarchyContainer/>
+      <HierarchyContainer :dataForHierarchy="hierarchyData"/>
     </OrgChartWrapper>
   </Layout>
 </template>
@@ -20,7 +20,78 @@ export default {
   },
   data() {
     return {
-      //
+      hierarchyData: {
+        name: "A1",
+        children: [
+          {
+            name: "B1",
+            children: [
+              {
+                name: "C1",
+                value: 100
+              },
+              {
+                name: "C2",
+                value: 300
+              },
+              {
+                name: "C3",
+                value: 200
+              }
+            ]
+          },
+          {
+            name: "B2",
+            value: 200,
+            children: [
+              {
+                name: "T1",
+                value: 100
+              },
+              {
+                name: "T2",
+                value: 300,
+                children: [
+                  {
+                    name: "Y1",
+                    value: 100
+                  },
+                  {
+                    name: "Y2",
+                    value: 300
+                  },
+                  {
+                    name: "Y3",
+                    value: 200
+                  },
+                  {
+                    name: "Y4",
+                    value: 200
+                  }
+                ]
+              },
+              {
+                name: "T3",
+                value: 200,
+                children: [
+                  {
+                    name: "X1",
+                    value: 100
+                  },
+                  {
+                    name: "X2",
+                    value: 300
+                  },
+                  {
+                    name: "X3",
+                    value: 200
+                  }
+                ]
+              }
+            ]
+          }
+        ]
+      }
     };
   }
 };
--- src/config.js
+++ src/config.js
@@ -1,2 +1,4 @@
 export const nodeWidth = 300;
 export const nodeHeight = 240;
+export const defaultNodeDensityX = 1.2;
+export const defaultNodeDensityY = 1.5;
