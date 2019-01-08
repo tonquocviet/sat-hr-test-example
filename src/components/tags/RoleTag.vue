50070758151b6c8404e8bea3d8e8cc5792943c14
Thuan Ho <hot@mti-tech.vn>
2019-01-08 12:10:53 +0700

    tách component tag ra

---
+++ src/components/tags/RoleTag.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-list-tile class="grow">
+    <v-btn
+      :color="tagStyle.color "
+      :class="`${tagStyle.textColor} font-weight-bold`"
+    >{{tagStyle.text}}</v-btn>
+  </v-list-tile>
+</template>
+
+<script>
+const tagTypeStyles = [
+  {
+    color: "warning",
+    textColor: "secondary--text",
+    text: "HUMAN RESOURCES"
+  },
+  {
+    color: "default",
+    textColor: "primary--text",
+    text: "MANAGER"
+  },
+  {
+    color: "error",
+    textColor: "white--text",
+    text: "CORPORATE"
+  }
+];
+export default {
+  props: {
+    tagType: Number
+  },
+  computed: {
+    tagStyle: function() {
+      return tagTypeStyles[this.tagType];
+    }
+  }
+};
+</script>
