ecea165a871161fc343fbde024acb0647a2787c4
Thuan Ho <hot@mti-tech.vn>
2019-01-14 14:43:54 +0700

    validate for corporate modal

--- src/components/forms/CorporateForm.vue
+++ src/components/forms/CorporateForm.vue
@@ -85,13 +85,15 @@ export default {
       this.$refs.form.resetValidation();
     },
     onSave() {
-      const { name, nodePosition, company } = this.nodeDataDetail;
-      const data = {
-        name: name,
-        company: company,
-        nodePosition: nodePosition
-      };
-      this.$emit("saveDetails", data);
+      if (this.$refs.form.validate()) {
+        const { name, nodePosition, company } = this.nodeDataDetail;
+        const data = {
+          name: name,
+          company: company,
+          nodePosition: nodePosition
+        };
+        this.$emit("saveDetails", data);
+      }
     },
     onCancel() {
       this.$emit("closeModal");
