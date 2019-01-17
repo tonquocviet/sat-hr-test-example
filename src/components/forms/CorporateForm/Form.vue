763184b0c85e2a3563508f52c0029ce630a0e3e0
Phạm Xuân Duy <xduy.pham@gmail.com>
2019-01-17 20:56:54 +0700

    ui Corporation Slide Info

---
+++ src/components/forms/CorporateForm/Form.vue
@@ -0,0 +1,39 @@
+<template>
+  <Editing
+    v-if="!isShowModal"
+    :nodeDataDetail="nodeDataDetail"
+    :apiEndPoints="apiEndPoints"
+    @saveDetails="saveDetails"
+    @closeModal="closeModal"
+  />
+
+  <Preview :editForm="editForm" :detailLink="detailLink" :nodeDataDetail="nodeDataDetail" v-else/>
+</template>
+<script>
+import Preview from "./Preview";
+import Editing from "./Editing";
+export default {
+  components: {
+    Preview,
+    Editing
+  },
+  props: {
+    nodeDataDetail: Object,
+    apiEndPoints: Object,
+    isShowModal: Boolean,
+    editForm: Function,
+    detailLink: String
+  },
+  methods: {
+    saveDetails(data) {
+      this.$emit("saveDetails", data);
+    },
+    closeModal() {
+      this.$emit("closeModal");
+    }
+  }
+};
+</script>
+
+<style scoped>
+</style>
---
+++ src/components/forms/CorporateForm/Preview.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-container>
+    <v-layout align-center justify-center>
+      <v-btn :href="detailLink + '/' + nodeDataDetail.id" target="blank" small color="default">Full</v-btn>
+      <v-btn @click="editForm" small color="primary">Edit</v-btn>
+    </v-layout>
+    <v-layout>
+      <v-flex xs12>
+        <h4>{{nodeDataDetail.name}}</h4>
+      </v-flex>
+    </v-layout>
+    <v-layout>
+      <v-flex xs12>
+        <h4>{{nodeDataDetail.nodePosition.name}}</h4>
+      </v-flex>
+    </v-layout>
+    <v-layout>
+      <v-flex xs12>
+        <p>{{nodeDataDetail.description}}</p>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+export default {
+  props: {
+    nodeDataDetail: Object,
+    editForm: Function,
+    detailLink: String
+  }
+};
+</script>
+
+<style scoped >
+</style>
