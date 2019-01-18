fe7b96312641d6982c33d504619dc7ee948b4e93
Phạm Xuân Duy <xduy.pham@gmail.com>
2019-01-18 11:39:09 +0700

    edit ui corporate details

--- src/components/forms/CorporateForm/Preview.vue
+++ src/components/forms/CorporateForm/Preview.vue
@@ -1,25 +1,41 @@
 <template>
-  <v-container>
-    <v-layout align-center justify-center>
-      <v-btn :href="detailLink + '/' + nodeDataDetail.id" target="blank" small color="default">Full</v-btn>
-      <v-btn @click="editForm" small color="primary">Edit</v-btn>
-    </v-layout>
-    <v-layout>
+  <v-form>
+    <v-container>
+      <v-layout align-center justify-center>
+        <div class="v-btn-full">
+          <v-btn
+            absolute
+            dark
+            small
+            fab
+            top
+            right
+            color="white"
+            :href="detailLink + '/' + nodeDataDetail.id"
+            target="blank"
+          >
+            <v-icon class="black--text">open_in_new</v-icon>
+          </v-btn>
+        </div>
+        <div class="v-btn-create">
+          <v-btn @click="editForm" absolute dark small fab top right color="green">
+            <v-icon>create</v-icon>
+          </v-btn>
+        </div>
+      </v-layout>
+      <v-layout>
+        <v-flex xs12>
+          <h3 class="success--text display-1 v-text-h3">{{nodeDataDetail.name}}</h3>
+        </v-flex>
+      </v-layout>
       <v-flex xs12>
-        <h4>{{nodeDataDetail.name}}</h4>
+        <h4>Description</h4>
       </v-flex>
-    </v-layout>
-    <v-layout>
-      <v-flex xs12>
-        <h4>{{nodeDataDetail.nodePosition.name}}</h4>
-      </v-flex>
-    </v-layout>
-    <v-layout>
       <v-flex xs12>
         <p>{{nodeDataDetail.description}}</p>
       </v-flex>
-    </v-layout>
-  </v-container>
+    </v-container>
+  </v-form>
 </template>
 
 <script>
@@ -31,3 +47,15 @@ export default {
   }
 };
 </script>
+<style>
+.v-text-h3 {
+  font-weight: bold;
+}
+.v-btn-full a {
+  top: 8px !important;
+}
+
+.v-btn-create button {
+  top: 70px !important;
+}
+</style>
