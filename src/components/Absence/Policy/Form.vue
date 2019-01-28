cc4a4a19e488b5579c304489c6a6d1d33228dc08
Thuan Ho <hot@mti-tech.vn>
2019-01-28 16:35:29 +0700

    refactor code

--- src/components/Absence/Policy/Form.vue
+++ src/components/Absence/Policy/Form.vue
@@ -1,15 +1,11 @@
 <template>
   <v-layout row>
-    <v-flex md9 class="mt-2">
+    <v-flex class="mt-2">
       <v-flex xs12 right class="right-button-container">
         <v-btn icon class="primary--text">
           <v-icon>filter_list</v-icon>
         </v-btn>
-        <v-btn
-          v-if="viewMode === 'card'"
-          icon
-          @click="changeViewMode(true)"
-        >
+        <v-btn v-if="viewMode === 'card'" icon @click="changeViewMode(true)">
           <v-icon>list</v-icon>
         </v-btn>
         <v-btn v-else icon @click="changeViewMode(false)">
@@ -18,28 +14,25 @@
       </v-flex>
       <v-flex xs12>
         <v-tabs color="transparent" dark slider-color="primary">
-          <v-tab v-for="item in itemList" :key="item.id" ripple class="primary--text">
-            {{ item.text }}
+          <v-tab v-for="(text,index) in tabs" :key="index" ripple class="primary--text">
+            {{ text }}
             <v-chip color="primary" text-color="white" small>{{ countPolicy }}</v-chip>
           </v-tab>
           <v-tab-item>
-            <PolicyTable @lengthPolicy="lengthPolicy" v-if="viewMode === 'list'" :apiPolicy="apiPolicy"/>
+            <PolicyTable
+              v-if="viewMode === 'list'"
+              :apiPolicy="apiPolicy"
+            />
           </v-tab-item>
-          <v-tab-item>
-            Page active
-          </v-tab-item>
-          <v-tab-item>
-            Page In active
-          </v-tab-item>
+          <v-tab-item>Page active</v-tab-item>
+          <v-tab-item>Page In active</v-tab-item>
         </v-tabs>
       </v-flex>
     </v-flex>
     <CreatePolicy :isShow="isShowCreate" @closeDialog="isShowCreate = false"></CreatePolicy>
-    <v-layout>
-      <v-btn @click="isShowCreate = true" color="success" class="create-policy mr-5 mb-5 " fab dark position absolute>
-        <v-icon>create</v-icon>
-      </v-btn>
-    </v-layout>
+    <v-btn @click="isShowCreate = true" color="error" fab dark fixed right bottom>
+      <v-icon>add</v-icon>
+    </v-btn>
   </v-layout>
 </template>
 <script>
@@ -53,27 +46,18 @@ export default {
   },
   props: {
     viewMode: String,
-    apiPolicy: Object,
+    apiPolicy: Object
   },
   methods: {
     changeViewMode(isListView) {
       this.$emit("changeViewMode", isListView ? "list" : "card");
-    },
-    lengthPolicy(item){
-      this.countPolicy = item;
-    },
+    }
   },
   data() {
     return {
-      pageIndex: 0,
-      loading: true,
-      countPolicy: 0,
+      countPolicy: 5,
       isShowCreate: false,
-      itemList: [
-        { text: "All" },
-        { text: "Active" },
-        { text: "Inactive" }
-      ],
+      tabs: ["All", "Active", "Inactive"]
     };
   }
 };
@@ -82,9 +66,5 @@ export default {
 .right-button-container {
   position: relative;
   z-index: 1;
-}
-.create-policy{
-  bottom: 0;
-  right: 0;
 }
 </style>
