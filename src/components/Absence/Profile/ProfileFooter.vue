c49a3d736193e1388bc38d798d222b38a46caa98
Lee Sin Jr <vanphong250895@gmail.com>
2019-01-28 17:20:21 +0700

    phong fix status item pass and upcomming

--- src/components/Absence/Profile/ProfileFooter.vue
+++ src/components/Absence/Profile/ProfileFooter.vue
@@ -1,23 +1,19 @@
 <template>
   <div>
     <h3 class="mt-3">Upcoming leaves</h3>
-    <v-card v-for="item in items" :key="item.id" class="mt-3">
-      <LeavePast :item="item"></LeavePast>
-    </v-card>
+    <LeaveRowItem v-for="item in items" :key="item.id" :item="item"></LeaveRowItem>
     <h3 class="mt-3">Past leaves</h3>
-    <v-card v-for="item in items" :key="item.id" class="mt-3">
-      <LeavePast :item="item"></LeavePast>
-    </v-card>
+    <LeaveRowItem v-for="item in items" :key="item.id" :item="item" :passLeave="true"></LeaveRowItem>
   </div>
 </template>
 <script>
-import LeavePast from './LeavePast'
+import LeaveRowItem from './LeaveRowItem'
 export default {
   props: {
     items: Array,
   },
   components: {
-    LeavePast
+    LeaveRowItem
   }
 };
 </script>
