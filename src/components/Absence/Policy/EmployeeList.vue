db7321466b00043f9982533440babe7164b04bdb
Lee Sin Jr <vanphong250895@gmail.com>
2019-01-28 16:40:41 +0700

    phong list employee

---
+++ src/components/Absence/Policy/EmployeeList.vue
@@ -0,0 +1,117 @@
+<template>
+  <v-layout row wrap>
+    <v-flex md9 sx12>
+      <v-flex xs12 right class="right-button-container mb-1">
+        <v-btn color="info">Add New Employee</v-btn>
+      </v-flex>
+      <v-data-table :headers="headers" :items="desserts" class="elevation-1">
+        <template slot="items" slot-scope="props">
+          <td>
+            <v-btn flat icon @click="absenceClick(props.item)" class="ma-0" color="black">
+              <v-icon>remove_circle</v-icon>
+            </v-btn>
+          </td>
+          <td>{{ props.item.name }}</td>
+          <td class="text-xs-left">{{ props.item.designation }}</td>
+          <td class="text-xs-left">{{ props.item.joined }}</td>
+          <td class="text-xs-left">{{ props.item.contact }}</td>
+          <td class="text-xs-left">{{ props.item.email }}</td>
+        </template>
+      </v-data-table>
+    </v-flex>
+    <v-flex md3 sx12 class="pl-3">
+      <v-layout>
+        <v-flex xs12 sm12 >
+          <v-card flat>
+            <h3 class="ml-2 pt-2">Tiêu đề</h3>
+          </v-card>
+          <v-card
+            flat
+            class="py-1"
+          >
+            <v-list two-line class="item-card-absence">
+              <v-list-tile>
+                <div class="v-image-user">
+                  <v-avatar>
+                    <img
+                      src="https://cdn.vuetifyjs.com/images/john.jpg"
+                      alt="John"
+                    >
+                  </v-avatar>
+                </div>
+                <v-list-tile-sub-title>
+                  <span class="font-weight-bold">name</span>
+                  <v-layout>
+                    <v-icon class="caption">date_range</v-icon>
+                    <span class="caption ml-1">start date</span>
+                    <v-icon class="caption ml-4">date_range</v-icon>
+                    <span class="caption ml-1">end date</span>
+                  </v-layout>
+                  <v-layout column>
+                    <span class="date-off error--text mr-3">dem ngay</span>
+                    <div>
+                      <v-chip
+                        class="mx-0 my-0"
+                        small
+                        text-color="white"
+                      >tag</v-chip>
+                    </div>
+                  </v-layout>
+                </v-list-tile-sub-title>
+              </v-list-tile>
+            </v-list>
+          </v-card>
+          <v-card flat>
+            <v-layout justify-end>
+              <v-btn  flat color="success">View full</v-btn>
+            </v-layout>
+          </v-card>
+        </v-flex>
+      </v-layout>
+    </v-flex>
+  </v-layout>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      headers: [
+        {
+          text: "",
+          align: "left",
+          sortable: false,
+          value: "name"
+        },
+        { text: "Name", value: "name", align: "left" },
+        { text: "Designation", value: "designation", align: "left" },
+        { text: "Joined", value: "joined", align: "left" },
+        { text: "Contact No.", value: "contact", align: "left" },
+        { text: "Email", value: "email", align: "left" }
+      ],
+      desserts: [
+        {
+          name: "Frozen Yogurt",
+          designation: 159,
+          joined: 6.0,
+          contact: 24,
+          email: "vanphong250895@gmail.com"
+        },
+        {
+          name: "Frozen Yogurt",
+          designation: 159,
+          joined: 6.0,
+          contact: 24,
+          email: "vanphong250895@gmail.com"
+        },
+        {
+          name: "Frozen Yogurt",
+          designation: 159,
+          joined: 6.0,
+          contact: 24,
+          email: "vanphong250895@gmail.com"
+        }
+      ]
+    };
+  }
+};
+</script>
--- src/components/Absence/Policy/Form.vue
+++ src/components/Absence/Policy/Form.vue
@@ -1,53 +1,54 @@
 <template>
-  <v-layout row>
-    <v-flex md9 class="mt-2">
-      <v-flex xs12 right class="right-button-container">
-        <v-btn color="info">Add New Policy</v-btn>
-        <v-btn icon class="primary--text">
-          <v-icon>filter_list</v-icon>
-        </v-btn>
-        <v-btn
-          v-if="viewMode === 'card'"
-          icon
-          @click="changeViewMode(true)"
-        >
-          <v-icon>list</v-icon>
-        </v-btn>
-        <v-btn v-else icon @click="changeViewMode(false)">
-          <v-icon>apps</v-icon>
-        </v-btn>
-      </v-flex>
-      <PolicyList v-if="viewMode === 'list'" :apiPolicy="apiPolicy"/>
-    </v-flex>
-  </v-layout>
+ <v-layout row wrap>
+   <v-flex xs12>
+     <v-tabs color="cyan" dark slider-color="yellow">
+       <v-tab v-for="item in itemList" :key="item.id" ripple>{{item.text}}</v-tab>
+       <v-tab-item>
+         tab overview
+       </v-tab-item>
+       <v-tab-item>
+         <EmployeeList></EmployeeList>
+       </v-tab-item>
+       <v-tab-item>Performance</v-tab-item>
+       <v-tab-item>Settings</v-tab-item>
+     </v-tabs>
+   </v-flex>
+ </v-layout>
 </template>
 <script>
 import PolicyList from "./PolicyList";
-
+import EmployeeList from "./EmployeeList";
 export default {
-  components: {
-    PolicyList,
-  },
-  props: {
-    viewMode: String,
-    apiPolicy: Object
-  },
-  methods: {
-    changeViewMode(isListView) {
-      this.$emit("changeViewMode", isListView ? "list" : "card");
-    }
-  },
-  data() {
-    return {
-      pageIndex: 0,
-      loading: true,
-    };
-  }
+ components: {
+   PolicyList,
+   EmployeeList
+ },
+ props: {
+   viewMode: String,
+   apiPolicy: Object
+ },
+ methods: {
+   changeViewMode(isListView) {
+     this.$emit("changeViewMode", isListView ? "list" : "card");
+   }
+ },
+ data() {
+   return {
+     pageIndex: 0,
+     loading: true,
+     itemList: [
+       { id: 1, text: "Overview" },
+       { id: 2, text: "Employee List" },
+       { id: 3, text: "Performance" },
+       { id: 4, text: "Settings" }
+     ]
+   };
+ }
 };
 </script>
 <style scoped>
 .right-button-container {
-  position: relative;
-  z-index: 1;
+ position: relative;
+ z-index: 1;
 }
 </style>
