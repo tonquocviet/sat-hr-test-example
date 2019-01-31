3e659ebac3a704eedfcf632f33fc9ddea4e03d62
duthaho <ohopoo@gmail.com>
2019-01-31 13:54:22 +0700

    Update PolicyAlert.vue

--- src/components/alerts/PolicyAlert.vue
+++ src/components/alerts/PolicyAlert.vue
@@ -2,9 +2,9 @@
   <v-flex>
     <v-layout v-for="item in dataAlerts" :key="item.id" row justify-center align-center>
       <v-flex xs1>
-        <v-icon v-if="item.type === 'error' " color="black">error</v-icon>
-        <v-icon v-if="item.type === 'info' " color="error">cancel</v-icon>
-        <v-icon v-if="item.type === 'success' " color="success">check_circle</v-icon>
+        <v-icon v-if="item.type === 'error'" color="black">error</v-icon>
+        <v-icon v-if="item.type === 'info'" color="error">cancel</v-icon>
+        <v-icon v-if="item.type === 'success'" color="success">check_circle</v-icon>
       </v-flex>
       <v-flex xs10>
         <div class="elevation-1">
