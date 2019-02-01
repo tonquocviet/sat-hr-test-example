dfda38279561029480454a50f8b10e8bb86a5903
Thuan Ho <hot@mti-tech.vn>
2019-02-01 16:43:12 +0700

    Tạm thời minh inject apiAbsence vào Vue, sẽ tìm cách edit sau.

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -1,3 +1,16 @@
 import Vue from "vue";
 import Axios from "axios";
 Vue.prototype.$http = Axios;
+const baseUrl =
+    "https://elastic-swanson-9b5efe.netlify.com/.netlify/functions/server/api";
+Vue.prototype.apiAbsence = {
+    filterAbsences: `${baseUrl}/absence/filter`,
+    filterWhoAbsencing: `${baseUrl}/absence/filter-absencing`,
+    filterUpcommingAbsence: `${baseUrl}/absence/filter-upcomming`,
+    getReason: `${baseUrl}/absence/get-reasons`,
+    approveRequest: `${baseUrl}/absence/approve-request`,
+    getAbsenceHRApprovers: absenceId => `${baseUrl}/absence/${absenceId}/approvers`,
+    getCommentAbsence: absenceId => `${baseUrl}/absence/${absenceId}/comments`,
+    getAbsencePolicies: absenceId => `${baseUrl}/absence/${absenceId}/policies`,
+    rejectRequest: `${baseUrl}/absence/reject-request`
+};
