ba6ee09b8a5538167d9dca7402c17b6bcea82d45
vuong2017 <phanquyenvuong@gmail.com>
2019-02-13 14:31:34 +0700

    fix-bug

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -2,19 +2,20 @@ import Vue from "vue";
 import Axios from "axios";
 Vue.prototype.$http = Axios;
 const baseUrl =
-    "https://elastic-swanson-9b5efe.netlify.com/.netlify/functions/server/api";
+  "https://elastic-swanson-9b5efe.netlify.com/.netlify/functions/server/api";
 Vue.prototype.apiAbsence = {
-    filterAbsences: `${baseUrl}/absence/filter`,
-    filterWhoAbsencing: `${baseUrl}/absence/filter-absencing`,
-    filterUpcommingAbsence: `${baseUrl}/absence/filter-upcomming`,
-    getReason: `${baseUrl}/absence/get-reasons`,
-    approveRequest: `${baseUrl}/absence/approve-request`,
-    getAbsenceHRApprovers: absenceId => `${baseUrl}/absence/${absenceId}/approvers`,
-    getCommentAbsence: absenceId => `${baseUrl}/absence/${absenceId}/comments`,
-    getAbsencePolicies: absenceId => `${baseUrl}/absence/${absenceId}/policies`,
-    rejectRequest: `${baseUrl}/absence/reject-request`
+  filterAbsences: `${baseUrl}/absence/filter`,
+  filterWhoAbsencing: `${baseUrl}/absence/filter-absencing`,
+  filterUpcommingAbsence: `${baseUrl}/absence/filter-upcomming`,
+  getReason: `${baseUrl}/absence/get-reasons`,
+  approveRequest: `${baseUrl}/absence/approve-request`,
+  getAbsenceHRApprovers: absenceId => `${baseUrl}/absence/${absenceId}/approvers`,
+  getCommentAbsence: absenceId => `${baseUrl}/absence/${absenceId}/comments`,
+  getAbsencePolicies: absenceId => `${baseUrl}/absence/${absenceId}/policies`,
+  rejectRequest: `${baseUrl}/absence/reject-request`
 };
 Vue.prototype.apiPolicy = {
-    filterPolicy: `${baseUrl}/absence-policy/filter`,
-    filterCountPolicy: `${baseUrl}/absence-policy/count`
-}
+  filterPolicy: `${baseUrl}/absence/filter`,
+  filterCountPolicy: `${baseUrl}/absence-policy/count`,
+  getCountries: `${baseUrl}/get-countries`
+};
